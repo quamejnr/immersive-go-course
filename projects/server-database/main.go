47e861b48615ccd1e577bd9c803c28a19d630ab5
Kwame Akuamoah-Boateng <kaboateng14@gmail.com>
2024-08-18 12:20:23 +0000

    remove comment

--- projects/server-database/main.go
+++ projects/server-database/main.go
@@ -29,21 +29,6 @@ type Image struct {
 
 func (app App) GetImages(w http.ResponseWriter, r *http.Request) {
 
-	// images := []Image{
-	// 	{
-	// 		Title:   "Sunset",
-	// 		AltText: "Clouds at sunset",
-	// 		URL:     "https://images.unsplash.com/photo-1506815444479-bfdb1e96c566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-	// 	},
-	// 	{
-	// 		Title:   "Mountain",
-	// 		AltText: "A mountain at sunset",
-	// 		URL:     "https://images.unsplash.com/photo-1540979388789-6cee28a1cdc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-	// 	},
-	// }
-
-	// get images
-
 	rows, err := app.db.Query(context.Background(), "SELECT title, url, alt_text FROM public.images")
 	if err != nil {
 		log.Printf("query error: %s", err.Error())
